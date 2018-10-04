ae9cc4eca58887c257c3e21b609c59e416f7ef07
nevliin <nevliin.white@gmail.com>
2018-10-04 23:28:45 +0200

    Added singleton initialization of AuthUtil

--- server/src/core/singletons.ts
+++ server/src/core/singletons.ts
@@ -1,11 +1,14 @@
 import {ErrorCodeUtil} from "../utils/error-code/error-code.util";
 import {LoggingUtil} from "../utils/logging/logging.util";
+import {AuthUtil} from "../utils/auth/auth.util";
 
 export class Singletons {
 
-    public static init() {
+    public static async init() {
         ErrorCodeUtil.init();
         LoggingUtil.init();
+        await AuthUtil.init();
+
     }
 
 }
