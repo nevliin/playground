e87ed21e237accd2534b3d024bbfca5f43cedc20
nevliin <nevliin.white@gmail.com>
2018-10-06 14:01:22 +0200

    Added route for changing passwords

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -3,6 +3,7 @@ import {AuthUtil} from "../utils/auth/auth.util";
 import {ISignUpModel} from "../utils/auth/signup.model";
 import {ErrorCodeUtil} from "../utils/error-code/error-code.util";
 import {ILoginModel} from "../utils/auth/login.model";
+import {IUpdatePasswordModel} from "../utils/auth/update-password.model";
 
 const express = require('express');
 
@@ -31,6 +32,16 @@ export const init = (): Router => {
         }
     });
 
+    authRouter.post('/updatePassword', async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const userId: number = await AuthUtil.updatePassword(<IUpdatePasswordModel>req.body);
+            res.status(200).send({
+                userId: userId
+            });
+        } catch (e) {
+            ErrorCodeUtil.resolveErrorOnRoute(e, res);
+        }
+    });
 
     return authRouter;
 };
