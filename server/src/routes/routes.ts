f8d9f08267faa0d5fc235d1a95b94b91866aaf0b
nevliin <nevliin.white@gmail.com>
2018-10-07 21:55:29 +0200

    Added router of CRUDConstructor under /test

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -2,6 +2,8 @@ import {Application, NextFunction, Request, Response, Router} from "express";
 import {TestService} from "../services/test.service";
 import {AuthUtil} from "../utils/auth/auth.util";
 import {authRouter} from "./auth-routes";
+import {CRUDConstructor} from "../core/crud-constructor";
+import {TestModel} from "../models/test.model";
 
 const express = require('express');
 export const router = express.Router();
@@ -12,11 +14,12 @@ export class Routes {
 
         const t: TestService = new TestService();
         const auth: AuthUtil = new AuthUtil();
+        const testModelCRUD: CRUDConstructor<TestModel> = new CRUDConstructor<TestModel>(new TestModel(), 'fin_category');
 
         app.use('/index', async (req: Request, res: Response, next: NextFunction) => {
             res.json(await t.getTest());
         });
 
-        app.use('/auth', authRouter);
+        app.use('/test', testModelCRUD.getRouter());
     }
 }
